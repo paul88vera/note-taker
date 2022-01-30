e508c678d9c02f70f605011b41b04422b8cd6273
paul88vera <paul88vera@gmail.com>
2022-01-30 02:51:59 -0600

    code condense / js link issue

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require('path');
 const util = require('util');
 const fs = require('fs');
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
-// Handling processes : Async & Util was found online and was easier for me to understand, and less typing
+// Handling processes : Async & Util was easier for me to understand, and less typing
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -31,7 +31,7 @@ app.post('/api/notes', (req,res) => {
  let newNote = req.body;
  readFileAsync('./db/db.json', 'utf-8').then(function(data) {
   const notes = [].concat(JSON.parse(data));
-  newNote.id = uuidv4();
+  newNote.id = [notes.length + 1].concat(uuidv4());
   notes.push(newNote);
   return notes;
  }).then(function(notes) {
@@ -39,6 +39,13 @@ app.post('/api/notes', (req,res) => {
   res.json(newNote);
  })
 });
+
+//TODO: Add "DELETE" request
+// app.delete('/api/notes/:id', (req,res) => {
+//  readFileAsync('./db.db.json', 'utf-8').then(function(data){
+//   const notes = [].concat(JSON.parse(data));
+//  })
+// });
 
 // HTML ROUTES //
 // displays notes.html
@@ -48,6 +55,11 @@ res.sendFile(path.join(__dirname, "../public/notes.html"));
 
 // displays index.html
 app.get("/", function(req, res) {
+ res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// displays index.html with anything after /api/notes/
+app.get("*", function(req, res) {
  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
